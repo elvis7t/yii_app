4b26438493502d838e17af6044a7d1e5b045f08d
DESKTOP-7FEBRI0\elvis <elvis7t@gmail.com>
2024-02-29 22:19:16 -0300

    feat(yii2-course) added route configuration

---
+++ app/config/url.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'man' => 'site/index',
+    'contact' => 'site/contact',
+    'login' => 'site/login',
+    // 'logout' => 'site/logout',
+    '' => 'site/index',
+
+    // 'POST register' => 'site/register'
+];
