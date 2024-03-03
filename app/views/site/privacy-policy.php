632fdbffadf1f3511e9ecd69b7a7c39fafebed3e
DESKTOP-7FEBRI0\elvis <elvis7t@gmail.com>
2024-03-03 11:43:58 -0300

    feat(yii2-course) added privacy policy page

---
+++ app/views/site/privacy-policy.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Portifolio';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Privacy Policy!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+        
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4 mb-3">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+           
+           
+            </div>
+        </div>
+
+    </div>
+</div>
