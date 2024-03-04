5aca5ed644ec1ba52541739b60f15df04266bd64
DESKTOP-7FEBRI0\elvis <elvis7t@gmail.com>
2024-03-04 08:54:59 -0300

    feat(yii2-course) added project table migration

---
+++ app/migrations/m240304_114814_create_project_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%project}}`.
+ */
+class m240304_114814_create_project_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%project}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'tech_stack' => $this->text()->notNull(),
+            'description' => $this->text()->notNull(),
+            
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%project}}');
+    }
+}
