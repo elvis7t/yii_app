b5dad64ebbec92305ace09c13212e6128c44079c
DESKTOP-7FEBRI0\elvis <elvis7t@gmail.com>
2024-03-05 19:25:25 -0300

    feat(yii2-course) added controller and view with gii

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -4,6 +4,16 @@ namespace app\controllers;
 
 class BlogController extends \yii\web\Controller
 {
+    public function actionHowToUseGii()
+    {
+        return $this->render('how-to-use-gii');
+    }
+
+    public function actionIAmStartingABlog()
+    {
+        return $this->render('i-am-starting-a-blog');
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
