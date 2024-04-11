014451ad34a7af7fbe7c0a5bdc6e7fd367bcd974
DESKTOP-7FEBRI0\elvis <elvis7t@gmail.com>
2024-04-11 18:42:02 -0300

    feat(yii2-course) refactor app

---
+++ advanced/backend/views/user/_form.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap5\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\User */
+/* @var $form yii\bootstrap4\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'status')->dropDownList(['10' => 'Ativo', '9' => 'Inativo'])->label("Active")->error() ?>
+    <?= $form->field($model, 'username')->textInput(['placeholder' => '@abstract', 'required' => true])->label("Name")->error(); ?>
+    <?= $form->field($model, 'email')->textInput(['placeholder' => 'name@domain.com'])->label("E-mail")->error() ?>
+
+    <?= $form->field($model, 'password')->passwordInput() ?>
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
